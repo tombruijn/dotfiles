7b0ad64c98c2562c47595362d6f32597e9738d39
Tom de Bruijn <tom@tomdebruijn.com>
2015-03-29 22:06:11 +0200

    Make `sub` cmd a symlink to bin instead of alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -28,7 +28,6 @@ fi;
 alias pgstart="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
 alias pgstop="pg_ctl -D /usr/local/var/postgres stop -s -m fast"
 alias redis="redis-server /usr/local/etc/redis.conf"
-alias sub="/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl"
 alias mou="open -a mou"
 
 # Git
