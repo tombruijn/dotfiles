a990e17f0f8266baf7d1ba2b931c7a3a1983c1c7
Tom de Bruijn <tom@tomdebruijn.com>
2015-08-15 15:18:51 +0200

    Update oh-my-zsh borrowed scripts

--- zsh/.zsh/termsupport.zsh
+++ zsh/.zsh/termsupport.zsh
@@ -1,13 +1,26 @@
-# Usage: title short_tab_title looooooooooooooooooooooggggggg_windows_title
-# http://www.faqs.org/docs/Linux-mini/Xterm-Title.html#ss3.1
-# Fully support screen, iterm, and probably most modern xterm and rxvt
-# Limited support for Apple Terminal (Terminal can't set window or tab separately)
+# Set terminal window and tab/icon title
+#
+# usage: title short_tab_title [long_window_title]
+#
+# See: http://www.faqs.org/docs/Linux-mini/Xterm-Title.html#ss3.1
+# Fully supports screen, iterm, and probably most modern xterm and rxvt
+# (In screen, only short_tab_title is used)
+# Source: https://github.com/robbyrussell/oh-my-zsh/blob/192de6bcffb0294e19f4203f6f7dc1a7f3e427be/lib/termsupport.zsh
 function title {
-  if [[ "$TERM" == "screen" ]]; then
-    print -Pn "\ek$1\e\\" # set screen hardstatus, usually truncated at 20 chars
-  elif [[ ($TERM =~ "^xterm") ]] || [[ ($TERM == "rxvt") ]] || [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
-    print -Pn "\e]2;$2\a" # set window name
-    print -Pn "\e]1;$1\a" # set icon (=tab) name (will override window name on broken terminal)
+  emulate -L zsh
+  setopt prompt_subst
+
+  [[ "$EMACS" == *term* ]] && return
+
+  # if $2 is unset use $1 as default
+  # if it is set and empty, leave it as is
+  : ${2=$1}
+
+  if [[ "$TERM" == screen* ]]; then
+    print -Pn "\ek$1:q\e\\" #set screen hardstatus, usually truncated at 20 chars
+  elif [[ "$TERM" == xterm* ]] || [[ "$TERM" == rxvt* ]] || [[ "$TERM" == ansi ]] || [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
+    print -Pn "\e]2;$2:q\a" #set window name
+    print -Pn "\e]1;$1:q\a" #set icon (=tab) name
   fi
 }
 
@@ -15,16 +28,21 @@ ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<" # 15 char left truncated PWD
 ZSH_THEME_TERM_TITLE_IDLE="%n@%m: %~"
 
 # Appears when you have the prompt
-function precmd {
-  if [ "$DISABLE_AUTO_TITLE" != "true" ]; then
-    title $ZSH_THEME_TERM_TAB_TITLE_IDLE $ZSH_THEME_TERM_TITLE_IDLE
-  fi
+function termsupport_precmd {
+  title $ZSH_THEME_TERM_TAB_TITLE_IDLE $ZSH_THEME_TERM_TITLE_IDLE
 }
 
 # Appears at the beginning of (and during) of command execution
-function preexec {
-  if [ "$DISABLE_AUTO_TITLE" != "true" ]; then
-    local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]} # cmd name only, or if this is sudo or ssh, the next cmd
-    title "$CMD" "%100>...>$2%<<"
-  fi
+function termsupport_preexec {
+  emulate -L zsh
+  setopt extended_glob
+
+  # cmd name only, or if this is sudo or ssh, the next cmd
+  local CMD=${1[(wr)^(*=*|sudo|ssh|mosh|rake|-*)]:gs/%/%%}
+  local LINE="${2:gs/%/%%}"
+
+  title '$CMD' '%100>...>$LINE%<<'
 }
+
+precmd_functions+=(termsupport_precmd)
+preexec_functions+=(termsupport_preexec)
