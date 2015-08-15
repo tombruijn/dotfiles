c1bc2e74948301494e7fb05d4ff3f036769c27e2
Tom de Bruijn <tom@tomdebruijn.com>
2015-08-15 14:14:43 +0200

    Cleanup zsh completions

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -4,21 +4,16 @@ export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 # See: man zshoptions
+unsetopt menu_complete # do not autoselect the first completion entry
+unsetopt flowcontrol
 setopt auto_cd # cd prefix not necessary to open a dir
 setopt multios # Multiple output
 setopt cdable_vars
 setopt extended_glob # Allow special chars as part of filename patterns
-
-unsetopt menu_complete # do not autoselect the first completion entry
-unsetopt flowcontrol
 setopt auto_menu # show completion menu on succesive tab press
 setopt complete_in_word
 setopt always_to_end
 
-# Tab-completion
-autoload -U compinit
-compinit
-setopt AUTO_MENU
-
 source "$ZSH_DIR/key-bindings.zsh"
+source "$ZSH_DIR/completions.zsh"
 source "$ZSH_DIR/history.zsh"
