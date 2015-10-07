eec516bc251fa073dc401e36daf97b9ad03da241
Tom de Bruijn <tom@tomdebruijn.com>
2015-10-07 22:58:52 +0200

    Update hammerspoon window management
    Less jumpy when moving windows

--- hammerspoon/window_management.lua
+++ hammerspoon/window_management.lua
@@ -49,8 +49,8 @@ hotkey.bind({"cmd", "alt"}, "[", function()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.x
-  f.y = max.y
+  f.x = 0
+  f.y = 0
   f.w = max.w / 2
   f.h = max.h
   win:setFrame(f)
@@ -67,12 +67,11 @@ hotkey.bind({"cmd", "alt"}, "]", function()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.x + (max.w / 2)
+  f.x = max.w / 2
   f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
+  f.x2 = max.x2
+  f.y2 = max.y2
   win:setFrame(f)
-  win:ensureIsInScreenBounds()
 end)
 
 -- Align window to top left of the screen
@@ -106,10 +105,9 @@ hotkey.bind({"ctrl", "alt"}, "]", function()
 
   f.x = max.w / 2
   f.y = 0
-  f.w = max.w / 2
+  f.x2 = max.x2
   f.h = max.h / 2
   win:setFrame(f)
-  win:ensureIsInScreenBounds()
 end)
 
 -- Move window to the display on the left
@@ -156,7 +154,6 @@ hotkey.bind({"ctrl", "alt"}, "-", function()
   end
 
   win:setFrame(f)
-  win:ensureIsInScreenBounds()
 end)
 
 -- Grow window
@@ -170,7 +167,10 @@ hotkey.bind({"ctrl", "alt"}, "=", function()
   local screen = win:screen()
   local max = screen:frame()
 
-  if f.x + f.w >= (max.w - 5) then
+  if f.x == 0 then
+    -- Aligned to the left
+    f.w = f.w + 20
+  elseif f.x + f.w >= (max.w - 5) then
     -- Aligned to the right
     f.w = f.w + 20
     f.x = max.w - f.w
@@ -179,10 +179,10 @@ hotkey.bind({"ctrl", "alt"}, "=", function()
     f.w = f.w + 20
     f.x = (max.w - f.w) / 2
   else
+    -- Everywhere else
     f.w = f.w + 20
     f.x = f.x - 10
   end
 
   win:setFrame(f)
-  win:ensureIsInScreenBounds()
 end)
