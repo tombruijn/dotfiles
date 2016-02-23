168656e0a4cc156fdd8a55904fe9f9e633e2b278
Tom de Bruijn <tom@tomdebruijn.com>
2016-02-23 22:48:18 +0100

    Prompt color updates based on last exit status

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -3,8 +3,8 @@ setopt PROMPT_SUBST
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[green]%} %{$fg[white]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg[green]%}%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}●%{$fg[green]%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}▪%{$fg[green]%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
 
-PROMPT='%{$fg[green]%}%2~$(git_custom_status) %{$fg[blue]%}› %{$reset_color%}'
+PROMPT='%{$fg[green]%}%2~$(git_custom_status) %(?.%{$fg[blue]%}.%{$fg[red]%})› %{$reset_color%}'
 RPS1=''
