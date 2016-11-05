77818e06c7e7e2c12d7654e194bb411c3eef355a
Tom de Bruijn <tom@tomdebruijn.com>
2016-11-05 17:14:26 +0100

    Add Docker shorthands

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -6,6 +6,25 @@ function mk {
   mkdir "$1" && cd "$1"
 }
 
+# Docker shorthands
+#
+# Mostly removal commands.
+
+# Stop all containers
+function dstop {
+  docker stop $(docker ps -a -q)
+}
+
+# Remove all docker containers
+function drm {
+  docker rm $(docker ps -a -q)
+}
+
+# Remove all docker images
+function drmi {
+  docker rmi $(docker images -q)
+}
+
 # AppSignal cd + function helper
 #
 # Usage:
