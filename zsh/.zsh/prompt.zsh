31fed733baac874fc2f94c665145ed2b36103c9a
Tom de Bruijn <tom@tomdebruijn.com>
2017-09-17 18:48:06 +0200

    Use a smaller dirty indicator in prompt
    The increased font size in combination with the square icon was too
    distracting, so use a smaller minus symbol instead.

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -3,7 +3,7 @@ setopt PROMPT_SUBST
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$reset_color%} "
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}▪"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}‑"
 ZSH_THEME_GIT_PROMPT_CLEAN=" "
 
 PROMPT='%{$fg[cyan]%}%2~$(git_custom_status)%(?.%{$fg[blue]%}.%{$fg[red]%})› %{$reset_color%}'
