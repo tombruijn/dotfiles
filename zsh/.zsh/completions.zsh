2e3800b87fa1bea57f0cc4b541810a49f29563d4
Tom de Bruijn <tom@tomdebruijn.com>
2018-05-20 14:13:07 +0200

    Speed up completion loading
    Remove some unused completions. Configure the rest to explode more
    things.

--- zsh/.zsh/completions.zsh
+++ zsh/.zsh/completions.zsh
@@ -1,35 +1,23 @@
 # Source for some of this:
-# https://github.com/robbyrussell/oh-my-zsh/blob/192de6bcffb0294e19f4203f6f7dc1a7f3e427be/lib/completion.zsh
+# https://github.com/robbyrussell/oh-my-zsh/blob/a600ab4b8578ca0a8e6c6dae0373033b9d8c201c/lib/completion.zsh
 
 autoload -U compinit
 compinit -i
 zmodload -i zsh/complist
 
-# case-insensitive (all),partial-word and then substring completion
-if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
-  zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-  unset CASE_SENSITIVE
-else
-  if [ "x$HYPHEN_INSENSITIVE" = "xtrue" ]; then
-    zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-    unset HYPHEN_INSENSITIVE
-  else
-    zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-  fi
-fi
-
+# Basic autocompletion
 zstyle ':completion:*:*:*:*:*' menu select
+# Case-insensitive (all), partial-word and then substring completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
 
-# don't prompt for a huge list use a page or menu
+# Don't prompt for a huge list use a page or menu
 # Source: https://github.com/twe4ked/dotfiles/blob/b97d56982fe93f732e8cd355a2772a261e82addd/shell/zsh/completion.zsh#L22
 zstyle ':completion:*:default' list-prompt '%S%M matches%s'
-
-# kill - show list of processes
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-zstyle ':completion:*:*:kill:*:processes' menu yes select
-zstyle ':completion:*:kill:*' force-list always
-zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
+zstyle ':completion:*:default' menu 'select=0'
 
 # Use caching so that commands like apt and dpkg complete are useable
 zstyle ':completion::complete:*' use-cache 1
-zstyle ':completion::complete:*' cache-path $ZSH/cache/
+zstyle ':completion::complete:*' cache-path $ZSH_DIR/.cache/
+
+# # Disable named-directories autocompletion
+zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
