tombruijn/dotfiles

commit 46253d5cd0f8494ae8d7aa4a9e13595dc0528172

Reorganize ZSH config
Merge files, put more in .zshrc.
Remove features I don't use.

I don't tweak my ZSH every day (anymore) so I don't remember what config
is in which file. Merging some files will make it a bit easier to
manage.

zsh/.zshrc
@@ -1,62 +1,91 @@
-ZSH_DIR="$HOME/.zsh" # Path to my custom ZSH directory
+###############################################################################
+# $PATH configuration
 
-# Ensure unique paths within PATH. Useful on `reload`
+## Ensure unique paths within $PATH. Useful on calling `reload`
 typeset -U path
 
-# `cd` can navigate to subdirectories in these directories from anywhere
-setopt auto_cd
-cdpath=(. .. ~/tombruijn ~/appsignal)
-
-# Config
-source "$ZSH_DIR/config.zsh"
-source "$ZSH_DIR/functions.zsh"
-
-# Git functions
-source "$ZSH_DIR/git.zsh"
-
-# Theme
-source "$ZSH_DIR/appearance.zsh"
-source "$ZSH_DIR/prompt.zsh"
-source "$ZSH_DIR/termsupport.zsh"
-
-# Aliases
-source "$ZSH_DIR/aliases.zsh"
-
-# Base paths
-export PATH="$HOME/.bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:./bin:$PATH"
+## Base paths
+export PATH="$HOME/.bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
+## GPG paths
 export PATH="/usr/local/opt/gnupg/bin:$PATH"
-
-# Heroku Toolbelt bin path
-export PATH="/usr/local/heroku/bin:$PATH"
-
-# Rust paths
+## Python paths
+export PATH="/usr/local/opt/python/libexec/bin/:$PATH"
+## Rust paths
 export PATH="$HOME/.cargo/bin:$PATH"
-
-# Ansible
+## Heroku Toolbelt bin path
+export PATH="/usr/local/heroku/bin:$PATH"
+## Ansible paths
 export PATH="/usr/local/opt/ansible@1.9/bin:$PATH"
 
-# chruby
-source /usr/local/opt/chruby/share/chruby/chruby.sh
-source /usr/local/opt/chruby/share/chruby/auto.sh
+###############################################################################
+# Configuration
 
-# Enable Elixir IEX history
+## Set language and encoding
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+## Configure ZSH prompt
+## See: man zshoptions
+unsetopt menu_complete # do not autoselect the first completion entry
+unsetopt flowcontrol
+setopt auto_cd # `cd` command not necessary to open a dir
+setopt multios # Enable multiple outputs
+setopt cdable_vars
+setopt extended_glob # Allow special chars as part of filename patterns
+setopt auto_menu # Show completion menu on succesive tab press
+setopt complete_in_word
+setopt always_to_end
+
+# Tools to use
+export EDITOR="nvim"
+export PAGER="less"
+export LESS="-R"
+
+## Configure cd quick paths
+## `cd` can navigate to subdirectories in these directories from anywhere
+cdpath=(. .. ~/tombruijn ~/appsignal)
+
+## Enable Elixir IEX history
 export ERL_AFLAGS="-kernel shell_history enabled"
 
+## Disable Homebrew analytics
 export HOMEBREW_NO_ANALYTICS=1
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
-export FZF_DEFAULT_COMMAND='ag -i --hidden --path-to-ignore ~/.ignore --no-color -g ""'
-
-# Load private credentials from ~/.extra file
+## Load private credentials from ~/.extra file
 if [ -f ~/.extra ]; then
   source ~/.extra
 fi
 
-# Configure GPG
+###############################################################################
+# Load additional configuration
+
+ZSH_DIR="$HOME/.zsh" # Path to my custom ZSH directory
+
+source "$ZSH_DIR/history.zsh"
+source "$ZSH_DIR/completions.zsh"
+source "$ZSH_DIR/functions.zsh"
+source "$ZSH_DIR/aliases.zsh"
+
+# Theme
+source "$ZSH_DIR/theme.zsh"
+source "$ZSH_DIR/termsupport.zsh"
+
+###############################################################################
+# Load tools
+
+## Configure GPG
 export GPG_TTY=$(tty)
 
+## Load FZF
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+export FZF_DEFAULT_COMMAND='ag -i --hidden --path-to-ignore ~/.ignore --no-color -g ""'
+
+## Load chruby version manager + auto detection
+source /usr/local/opt/chruby/share/chruby/chruby.sh
+source /usr/local/opt/chruby/share/chruby/auto.sh
+
+## Load ASDF version manager
 if [ -d $HOME/.asdf ]; then
   . $HOME/.asdf/asdf.sh
-  . $HOME/.asdf/completions/asdf.bash
 fi

zsh/.zsh/appearance.zsh
@@ -1,10 +1,0 @@
-# Activate colors
-export TERM=xterm-256color
-autoload colors
-colors
-BASE16_SHELL="$HOME/.zsh/colors.sh"
-[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
-
-# Set ls colors
-export LSCOLORS="Gxfxcxdxbxegedabagacad"
-ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'

zsh/.zsh/config.zsh
@@ -1,24 +1,0 @@
-# Language
-export LANG=en_US.UTF-8
-export LC_CTYPE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-
-# Tools to use
-export EDITOR="nvim"
-export PAGER="less"
-export LESS="-R"
-
-# See: man zshoptions
-unsetopt menu_complete # do not autoselect the first completion entry
-unsetopt flowcontrol
-setopt auto_cd # cd prefix not necessary to open a dir
-setopt multios # Multiple output
-setopt cdable_vars
-setopt extended_glob # Allow special chars as part of filename patterns
-setopt auto_menu # show completion menu on succesive tab press
-setopt complete_in_word
-setopt always_to_end
-
-source "$ZSH_DIR/key-bindings.zsh"
-source "$ZSH_DIR/completions.zsh"
-source "$ZSH_DIR/history.zsh"

zsh/.zsh/git.zsh
@@ -1,19 +1,0 @@
-# Show git branch/tag, or name-rev if on detached head
-parse_git_branch() {
-  (command git symbolic-ref -q HEAD || command git name-rev --name-only --no-undefined --always HEAD) 2>/dev/null
-}
-
-# Show icon when there are uncommitted changes
-parse_git_dirty() {
-  if command git diff-index --quiet HEAD 2> /dev/null; then
-    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
-  else
-    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
-  fi
-}
-
-# If in a git repo, show dirty indicator + git branch
-git_custom_status() {
-  local git_where="$(parse_git_branch)"
-  [ -n "$git_where" ] && echo "$ZSH_THEME_GIT_PROMPT_PREFIX${git_where#(refs/heads/|tags/)}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX" || echo " "
-}

zsh/.zsh/key-bindings.zsh
@@ -1,10 +1,11 @@
-bindkey -e
+# iTerm keybindings
+bindkey -e # Emacs mode
 bindkey '\ew' kill-region
 
 # Ctrl + r/s - incremental search
 bindkey "^r" history-incremental-search-backward
 
-# ?
+# TODO: document
 bindkey "^[[5~" up-line-or-history
 bindkey "^[[6~" down-line-or-history
 

zsh/.zsh/prompt.zsh
@@ -1,10 +1,0 @@
-# Allow prompt parameter expansion
-setopt PROMPT_SUBST
-
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$reset_color%} "
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}‑"
-ZSH_THEME_GIT_PROMPT_CLEAN=" "
-
-PROMPT='%{$fg[cyan]%}%2~$(git_custom_status)%(?.%{$fg[blue]%}.%{$fg[red]%})› %{$reset_color%}'
-RPS1=''

zsh/.zsh/theme.zsh
@@ -1,0 +1,48 @@
+###############################################################################
+# Color configuration
+export TERM=xterm-256color
+autoload colors
+colors
+BASE16_SHELL="$HOME/.zsh/colors.sh"
+[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
+
+## Set ls colors
+export LSCOLORS="Gxfxcxdxbxegedabagacad"
+ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
+
+###############################################################################
+# Git helper functions
+
+## Show git branch/tag, or name-rev if on detached head
+parse_git_branch() {
+  (command git symbolic-ref -q HEAD || command git name-rev --name-only --no-undefined --always HEAD) 2>/dev/null
+}
+
+## Show icon when there are uncommitted changes
+parse_git_dirty() {
+  if command git diff-index --quiet HEAD 2> /dev/null; then
+    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+  else
+    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+  fi
+}
+
+## If in a git repo, show dirty indicator + git branch
+git_custom_status() {
+  local git_where="$(parse_git_branch)"
+  [ -n "$git_where" ] && echo "$ZSH_THEME_GIT_PROMPT_PREFIX${git_where#(refs/heads/|tags/)}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX" || echo " "
+}
+
+###############################################################################
+# Prompt definition
+
+setopt PROMPT_SUBST # Allow prompt configuration
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}‑"
+ZSH_THEME_GIT_PROMPT_CLEAN=" "
+# Left hand side
+PROMPT='%{$fg[cyan]%}%2~$(git_custom_status)%(?.%{$fg[blue]%}.%{$fg[red]%})› %{$reset_color%}'
+# Right hand side
+RPS1=''

commit 3fa6f5a646822e70727f9e8fc0694db91223355a

Fix missing import
Broke in 46253d5cd0f8494ae8d7aa4a9e13595dc0528172

zsh/.zshrc
@@ -63,6 +63,7 @@ fi
 ZSH_DIR="$HOME/.zsh" # Path to my custom ZSH directory
 
 source "$ZSH_DIR/history.zsh"
+source "$ZSH_DIR/key-bindings.zsh"
 source "$ZSH_DIR/completions.zsh"
 source "$ZSH_DIR/functions.zsh"
 source "$ZSH_DIR/aliases.zsh"