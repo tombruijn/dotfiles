7d3c0f277a20320f6eb376f10dace176968fd63d
Tom de Bruijn <tom@tomdebruijn.com>
2022-01-23 10:30:03 +0100

    Load Homebrew ZSH autocompletion
    Some programs installed through Homebrew have additional autocompletion
    shipped with it. Load them to improve the user experience.

--- zsh/.zsh/completions.zsh
+++ zsh/.zsh/completions.zsh
@@ -1,6 +1,10 @@
 # Source for some of this:
 # https://github.com/robbyrussell/oh-my-zsh/blob/a600ab4b8578ca0a8e6c6dae0373033b9d8c201c/lib/completion.zsh
 
+if type brew &>/dev/null; then
+  export FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+fi
+
 autoload -U compinit
 compinit -i
 zmodload -i zsh/complist
