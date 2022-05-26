cbb92ff57eae29109540b4eb6cf0929ddfbac775
Tom de Bruijn <tom@tomdebruijn.com>
2022-05-26 21:20:23 +0200

    Remove custom color config from ZSH config
    I added this way back in 2015 to match the tig bars with my vim theme in
    commit 4c4e8f3a7044b1ed98045c6b8e6852936c4a988b. The commit doesn't list
    any sources why or how this works. I looked at the docs and it seems
    like a lot of work and config for such as small thing.
    
    I've removed it in favor of using one of the existing 256 colors. The
    theme doesn't match anymore, but that's not that important.
    
    These additional prints at the start of the ZSH shell ended up in some
    output of commands I want to use,
    e.g. `chruby-exec ruby-3.1 -- echo 123 | pbcopy`
    It's too much hassle to work around it, so remove it.

--- zsh/.zsh/theme.zsh
+++ zsh/.zsh/theme.zsh
@@ -3,8 +3,6 @@
 export TERM=xterm-256color
 autoload colors
 colors
-BASE16_SHELL="$HOME/.zsh/colors.sh"
-[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
 
 ## Set ls colors
 export LSCOLORS="Gxfxcxdxbxegedabagacad"
