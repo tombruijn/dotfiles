f3aaaf11e6bbf7aef2d122b2524f1ead768ae0b9
Tom de Bruijn <tom@tomdebruijn.com>
2022-12-02 11:02:36 +0100

    Restore top left and right 25% window layouts
    Allow me to quickly align windows to the top left or right with only
    half height window size.
    
    I didn't reimplement these window layouts during the last refactor.

--- hammerspoon/window_management.lua
+++ hammerspoon/window_management.lua
@@ -9,12 +9,14 @@ window.animationDuration = 0
 local positions = {
   fullscreen = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 },
 
-  right50  = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
-  left50   = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
-  top50    = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
-  bottom50 = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
-  right33  = { x = (1 / 3) * 2, y = 0.00, x2 = 1.00, h = 1.00 },
-  left33   = { x = 0.00, y = 0.00, w = 1 / 3, h = 1.00 },
+  right50    = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
+  left50     = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
+  top50      = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
+  bottom50   = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
+  right33    = { x = (1 / 3) * 2, y = 0.00, x2 = 1.00, h = 1.00 },
+  left33     = { x = 0.00, y = 0.00, w = 1 / 3, h = 1.00 },
+  topLeft25  = { x = 0.00, y = 0.00, w = 0.5, h = 0.5 },
+  topRight25 = { x = 0.5, y = 0.0, w = 0.5, h = 0.5 },
 
   bottomCenter50 = { x = 0.25, y = 0.50, w = 0.50, h = 0.50 }
 }
@@ -54,6 +56,14 @@ end)
 
 keys.bindKeyFor("Right 33% window", function()
   move(positions.right33)
+end)
+
+keys.bindKeyFor("Top left 25% window", function()
+  move(positions.topLeft25)
+end)
+
+keys.bindKeyFor("Top right 25% window", function()
+  move(positions.topRight25)
 end)
 
 keys.bindKeyFor("Bottom center 25% window", function()
