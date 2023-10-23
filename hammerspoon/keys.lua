7133bc794a0a5b2178a7ee26422d7f92daf5a894
Tom de Bruijn <tom@tomdebruijn.com>
2023-10-23 21:29:18 +0200

    Add Proton Mail app open shortcut

--- hammerspoon/keys.lua
+++ hammerspoon/keys.lua
@@ -56,6 +56,7 @@ Keys = {
     ["Todoist"] = {{{"ctrl"}, "5"}, {hyper, "J"}},
     ["Visual Studio Code"] = {nil, {hyper, "T"}},
     ["Keynote"] = {{{"ctrl"}, "8"}, {hyper, "U"}},
+    ["Proton Mail"] = {{{"ctrl"}, "7"}, {hyper, "H"}},
   },
 }
 
