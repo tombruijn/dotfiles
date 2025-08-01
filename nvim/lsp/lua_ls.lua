09b7ddfe477ea2ca869e763aaf07328f7c44bf31
Tom de Bruijn <tom@tomdebruijn.com>
2025-08-01 21:24:59 +0200

    Define lua globals via the luarc file
    Instead of defining them as globals for every lua file.

--- nvim/lsp/lua_ls.lua
+++ nvim/lsp/lua_ls.lua
@@ -1,9 +1,6 @@
 return {
   settings = {
     Lua = {
-      diagnostics = {
-        globals = { "vim", "Snacks" }, -- when working on nvim config
-      },
       format = {
         enable = false, -- Rely on stylua
       },
