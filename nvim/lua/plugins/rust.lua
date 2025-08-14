fbcfdeefb6d36ed8d5d94de639aff2d44e5c1660
Tom de Bruijn <tom@tomdebruijn.com>
2025-08-14 20:41:39 +0200

    Add rust-analyzer config for neovim
    Add additional config to improve the LSP integration for rust.

--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -1,3 +1,53 @@
+vim.g.rustaceanvim = {
+  server = {
+    on_attach = function(_, bufnr)
+      vim.keymap.set("n", "<leader>cR", function()
+        vim.cmd.RustLsp("codeAction")
+      end, { desc = "Code Action", buffer = bufnr })
+    end,
+    default_settings = {
+      ["rust-analyzer"] = {
+        cargo = {
+          features = true,
+          loadOutDirsFromCheck = true,
+          buildScripts = {
+            enable = true,
+          },
+          extraEnv = { CARGO_TARGET_DIR = "tmp/rust_analyzer_target" },
+        },
+        check = {
+          command = "clippy",
+          extraArgs = { "--no-deps" },
+        },
+        files = {
+          watcher = "server",
+          excludeDirs = {
+            ".direnv",
+            ".git",
+            ".github",
+            ".gitlab",
+            ".venv",
+            "bin",
+            "node_modules",
+            "script",
+            "target",
+            "venv",
+            -- Custom cache directories
+            "rust_analyzer_target",
+            "clippy_local",
+          },
+        },
+      },
+    },
+  },
+  tools = {
+    code_actions =  {
+      -- use vim.ui for code actions
+      ui_select_fallback = true
+    }
+  }
+}
+
 return {
   {
     "mrcjkb/rustaceanvim",
