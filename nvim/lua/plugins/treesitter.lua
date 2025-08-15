ace88e60f53d521a79ea165c87906515d564b6d8
Tom de Bruijn <tom@tomdebruijn.com>
2025-08-15 22:42:12 +0200

    Activate syntax highlighting with treesitter
    It didn't work for Elixir. Pass in all the parsers as patterns to make
    it work. It might overkill, but it will make it work for now.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,41 +3,57 @@ return {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
     build = ":TSUpdate",
-    config = function()
+    lazy = false,
+    opts = {
+      parsers = {
+        bash = { "sh", "bash" },
+        c = { "c", "h" },
+        diff = { "diff" },
+        javascript = { "js", "jsx" },
+        json = { "json" },
+        jsonc = { "jsonc" },
+        lua = { "lua" },
+        markdown = { "md", "markdown" },
+        markdown_inline = {},
+        regex = { "regex" },
+        ruby = { "rb", "ruby" },
+        toml = { "toml" },
+        typescript = { "ts", "tsx" },
+        xml = { "xml" },
+        yaml = { "yaml", "yml" },
+
+        -- Elixir
+        elixir = { "ex", "exs" },
+        heex = { "heex" },
+        eex = { "eex" },
+
+        -- Python
+        python = { "py" },
+        ninja = { "ninja" },
+        rst = { "rst" },
+
+        -- Rust
+        rust = { "rs" },
+        ron = { "ron" },
+      },
+    },
+    config = function(_, opts)
+      local parsers = {}
+      for parser_name, _extensions in pairs(opts.parsers) do
+        table.insert(parsers, parser_name)
+      end
+
       require("nvim-treesitter")
-        .install({
-          "bash",
-          "c",
-          "diff",
-          "javascript",
-          "json",
-          "jsonc",
-          "lua",
-          "markdown",
-          "markdown_inline",
-          "python",
-          "regex",
-          "ruby",
-          "toml",
-          "ts",
-          "typescript",
-          "xml",
-          "yaml",
-
-          -- Elixir
-          "elixir",
-          "heex",
-          "eex",
-
-          -- Python
-          "ninja",
-          "rst",
-
-          -- Rust
-          "rust",
-          "ron",
-        })
+        .install(parsers)
         :wait(300000) -- wait max. 5 minutes
+
+      -- Extract parser names as filetypes (most parser names match filetype names)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = parsers,
+        callback = function()
+          vim.treesitter.start()
+        end,
+      })
     end,
   },
 }
