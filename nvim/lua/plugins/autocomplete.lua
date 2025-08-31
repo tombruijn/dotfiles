a80aa26d8b577924ed0d606be739001d3f24f86d
Tom de Bruijn <tom@tomdebruijn.com>
2025-08-31 15:04:52 +0200

    Add missing cmdline cmp plugin
    The cmdline setup doesn't work without it.

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -42,6 +42,7 @@ return {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-nvim-lsp-document-symbol",
+      "hrsh7th/cmp-cmdline",
       "L3MON4D3/LuaSnip",
     },
     opts = function()
