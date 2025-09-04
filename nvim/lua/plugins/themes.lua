b188db52e6b75b21798b2c9f4937f53e7423c43e
Tom de Bruijn <tom@tomdebruijn.com>
2025-09-04 22:40:26 +0200

    Simplify neovim theme config
    Remove some unnecessary additional config.
    Also configure comments and keywords to not be italic.

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -3,59 +3,45 @@ return {
     "folke/tokyonight.nvim",
     lazy = true,
     priority = 1000,
-    opts = {
-      style = "night",
-      dim_inactive = true,
-      on_colors = function(colors)
-        colors.bg = "#000000"
-        colors.bg_dark = "#1E1E1E" -- Inactive buffer
-      end,
-      on_highlights = function(hl, c)
-        hl["@markup.raw.markdown_inline"] = { bg = "NONE", fg = c.green2 }
-      end,
-    },
-    config = function(_, _opts)
-      local tokyonight = require("tokyonight")
+    config = function(_, _)
       local current_theme = "night"
       local zen_mode = false
 
-      -- Theme configurations
-      local theme_configs = {
-        night = {
-          style = "night",
-          dim_inactive = true,
-          on_colors = function(colors)
+      require("tokyonight").setup({
+        style = "night",
+        dim_inactive = true,
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false },
+        },
+        on_colors = function(colors)
+          if current_theme == "night" then
             colors.bg = "#000000"
             colors.bg_dark = zen_mode and "#000000" or "#1E1E1E"
             colors.comment = "#6B7280" -- Lighter gray for comments
-          end,
-          on_highlights = function(hl, c)
-            hl["@markup.raw.markdown_inline"] = { bg = "NONE", fg = c.green2 }
-          end,
-        },
-        day = {
-          dim_inactive = true,
-          on_colors = function(colors)
+          else
             colors.bg = "#f1f1f1"
             colors.bg_dark = zen_mode and "#f1f1f1" or "#e3e3e3"
-          end,
-        },
-      }
+          end
+        end,
+        on_highlights = function(hl, c)
+          hl["@markup.raw.markdown_inline"] = { bg = "NONE", fg = c.green2 }
+        end,
+      })
 
       -- Apply theme configuration
       local function apply_theme(theme)
-        tokyonight.setup(theme_configs[theme])
-        vim.cmd.colorscheme("tokyonight-" .. theme)
         current_theme = theme
+        vim.cmd.colorscheme("tokyonight-" .. theme)
       end
 
-      -- Toggle zen mode background matching
+      -- Toggle zen mode and apply the right theme
       local function set_zen_mode(enabled)
         zen_mode = enabled
         apply_theme(current_theme)
       end
 
-      -- Expose functions globally
+      -- Expose functions globally so we can call them in snacks.lua
       _G.tokyonight_theme = {
         current = function()
           return current_theme
@@ -74,9 +60,6 @@ return {
         -- Otherwise the background doesn't fully update to the new color
         vim.cmd("mode")
       end
-
-      -- Load initial theme
-      apply_theme("night")
 
       vim.keymap.set("n", "<leader>W", toggle_theme, { desc = "Toggle between tokyonight themes" })
     end,
