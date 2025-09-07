a194c1dbb92a0e09b1eb72f61dfd1a1f0ee0c060
Tom de Bruijn <tom@tomdebruijn.com>
2025-09-07 20:51:56 +0200

    Fix deprecated function call
    Use the method call instead.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -21,7 +21,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
     -- Enable inlay hints per buffer if the server supports them
     local client = vim.lsp.get_client_by_id(event.data.client_id)
-    if client and client.supports_method("textDocument/inlayHint") then
+    if client and client:supports_method("textDocument/inlayHint") then
       vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
     end
   end,
